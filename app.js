3e774315be7d6d18560a818b3fa4bafa6f1897b0
JuanSilva27 <juan.silva.0270@gmail.com>
2023-01-26 17:39:58 -0300

    se crea la estructura del backend y la la estructura del Frontend

--- app.js
+++ app.js
@@ -1,28 +1,17 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 var logger = require('morgan');
+
 const auth = require("./routes/auth")
 const users = require("./routes/users")
 const projects = require("./routes/projects")
 const task = require("./routes/task")
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 app.use("/api/auth",auth)
 app.use("/api/users",users)
