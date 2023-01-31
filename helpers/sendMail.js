a4626038ae9ef8be1c3f3e7c27e536a5cfd5ebc6
JuanSilva27 <juan.silva.0270@gmail.com>
2023-01-31 17:54:38 -0300

    se substituye el uso de nodemailer por SendGrid

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -1,30 +1,23 @@
-const nodemailer = require("nodemailer");
-
-const transport = nodemailer.createTransport({
-  host: process.env.HOST_MAIL,
-  port: process.env.PORT_MAIL,
-  auth: {
-    user: process.env.USER_MAIL,
-    pass: process.env.PASS_MAIL,
-  },
-});
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_APIKEY);
 
 module.exports = {
   confirmRegister: async (data) => {
     const { name, email, token } = data;
-
+    const msg = {
+      to: email, // Change to your recipient
+      from: process.env.SENDGRID_MAIL, // Change to your verified sender
+      subject: "Confirma tu cuenta",
+      text: "Confirma tu cuenta en Project Manager",
+      html: `
+      <p> Hola ${name}, haz click en el siguiente enlace para confirmar tu registro: <p/>
+      <a href="${process.env.URL_FRONT}/confirm/${token}">Confirma tu cuenta </a>
+      `,
+    };
     try {
-      const infoMail = await transport.sendMail({
-        from: "Project Manager <info@projectManager.com>",
-        to: email,
-        subject: "Confirma tu cuenta",
-        text: "Confirma tu cuenta en Project Manager",
-        html: `
-                <p> Hola ${name}, haz click en el siguiente enlace para confirmar tu registro: <p/>
-                <a href="${process.env.URL_FRONT}/confirm/${token}">Confirma tu cuenta </a>
-                `,
-      });
+      await sgMail.send(msg)
       console.log("Mail enviado");
+
     } catch (error) {
       console.log(error);
     }
@@ -32,19 +25,20 @@ module.exports = {
 
   frogotPassword: async (data) => {
     const { name, email, token } = data;
+    const msg = {
+      to: email, // Change to your recipient
+      from: process.env.SENDGRID_MAIL, // Change to your verified sender
+      subject: "Reestablece tu contraseña",
+      text: "Reestablece tu contraseña en Project Manager",
+      html: `
+      <p> Hola ${name}, haz click en el siguiente enlace para reestablecer tu contraseña: <p/>
+      <a href="${process.env.URL_FRONT}/confirm/${token}">Reestablece tu contraseña</a>
+      `,
+    };
 
     try {
-      const infoMail = await transport.sendMail({
-        from: "Project Manager <info@projectManager.com>",
-        to: email,
-        subject: "Reestablece tu contraseña",
-        text: "Reestanlece tu contraseña en Project Manager",
-        html: `
-                <p> Hola ${name}, haz click en el siguiente enlace para reestablecer tu contraseña: <p/>
-                <a href="${process.env.URL_FRONT}/confirm/${token}">Reestablece tu contraseña</a>
-                `,
-      });
-      console.log("aaaaaaaaaaaaaaaaa");
+      await sgMail.send(msg)
+      console.log("Mail enviado");
     } catch (error) {
       console.log(error);
     }
